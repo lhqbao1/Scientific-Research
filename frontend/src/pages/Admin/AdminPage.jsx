96514649fd29179214deef3bf56af9096d61ffad
DevCuongBui <cuong82434@st.vimaru.edu.vn>
2023-07-11 14:49:13 +0700

    add: auth login

--- frontend/src/pages/Admin/AdminPage.jsx
+++ frontend/src/pages/Admin/AdminPage.jsx
@@ -1,19 +1,20 @@
-import AdminHeader from "../../components/Admin/AdminHeader"
+import AdminHeader from "../../components/Admin/AdminHeader";
 import {
-    AppstoreOutlined,
-    ContainerOutlined,
-    MenuFoldOutlined,
-    PieChartOutlined,
-    DesktopOutlined,
-    MailOutlined,
-    MenuUnfoldOutlined,
-    UsergroupAddOutlined,
-    UserAddOutlined,
-    RightCircleOutlined,
-    CalendarOutlined
-} from '@ant-design/icons';
-import { Breadcrumb, Button, Col, Layout, Menu, Row, Slider } from 'antd';
-import { useState } from 'react';
+  AppstoreOutlined,
+  ContainerOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuUnfoldOutlined,
+  UsergroupAddOutlined,
+  UserAddOutlined,
+  RightCircleOutlined,
+  CalendarOutlined,
+} from "@ant-design/icons";
+import { Breadcrumb, Button, Col, Layout, Menu, Row, Slider } from "antd";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import AdminDashboard from "./AdminDashboard";
 import { Content, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
@@ -22,156 +23,170 @@ import ManageLecturer from "./ManageLecturer";
 import ManageInstructor from "./ManageInstructor";
 import ManageTopic from "./ManageTopic";
 import ManageSchedule from "./ManageSchedule";
-
+import {
+  clearCookie,
+  getToken,
+  getUserData,
+  setCartCookies,
+} from "../../lib/utils";
 
 const AdminPage = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  const [openManageSetudent, setOpenManageStudent] = useState(false);
+  const [openAD, setOpenAD] = useState(true);
+  const [openManageLecturer, setOpenManageLecturer] = useState(false);
+  const [openManageInstructor, setOpenManageInstructor] = useState(false);
+  const [openManageTopic, setOpenManageTopic] = useState(false);
+  const [openManageSchedule, setOpenManageSchedule] = useState(false);
 
-    const [collapsed, setCollapsed] = useState(true);
-    const [openManageSetudent, setOpenManageStudent] = useState(false)
-    const [openAD, setOpenAD] = useState(true)
-    const [openManageLecturer, setOpenManageLecturer] = useState(false)
-    const [openManageInstructor, setOpenManageInstructor] = useState(false)
-    const [openManageTopic, setOpenManageTopic] = useState(false)
-    const [openManageSchedule, setOpenManageSchedule] = useState(false)
+  const navigate = useNavigate();
 
-
-    function getItem(label, key, icon, children, type) {
-        return {
-            key,
-            icon,
-            children,
-            label,
-            type,
-        };
+  const token = getToken();
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
     }
-    const items = [
-        getItem('Dashboard', '1', <PieChartOutlined />),
-        getItem('Manage user', 'sub1', <UserAddOutlined />, [
-            getItem('Student', '2', <RightCircleOutlined />),
-            getItem('Lecturer', '3', <RightCircleOutlined />),
-            getItem('Instructors', '4', <RightCircleOutlined />),
-        ]),
-        getItem('Manage topic', '5', <ContainerOutlined />),
-        getItem('Manage council', 'sub2', <UsergroupAddOutlined />, [
-            getItem('Censorship council', '6', <RightCircleOutlined />),
-            getItem('Acceptance council', '7', <RightCircleOutlined />),
-        ]),
-        getItem('Manage schedule', '8', <CalendarOutlined />),
+  }, [navigate, token]);
 
+  const userData = getUserData();
 
-    ];
+  function getItem(label, key, icon, children, type) {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      type,
+    };
+  }
+  const items = [
+    getItem("Dashboard", "1", <PieChartOutlined />),
+    getItem("Manage user", "sub1", <UserAddOutlined />, [
+      getItem("Student", "2", <RightCircleOutlined />),
+      getItem("Lecturer", "3", <RightCircleOutlined />),
+      getItem("Instructors", "4", <RightCircleOutlined />),
+    ]),
+    getItem("Manage topic", "5", <ContainerOutlined />),
+    getItem("Manage council", "sub2", <UsergroupAddOutlined />, [
+      getItem("Censorship council", "6", <RightCircleOutlined />),
+      getItem("Acceptance council", "7", <RightCircleOutlined />),
+    ]),
+    getItem("Manage schedule", "8", <CalendarOutlined />),
+  ];
 
-    const openItems = (key) => {
-        if (key.key === '1') {
-            setOpenAD(!openAD)
-            setOpenManageStudent(false)
-            setOpenManageLecturer(false)
-            setOpenManageInstructor(false)
-            setOpenManageTopic(false)
+  const onSignOut = () => {
+    clearCookie();
+    navigate("/login");
+  };
 
-
-        }
-        if (key.key === '2') {
-            setOpenManageStudent(!openManageSetudent)
-            setOpenAD(false)
-            setOpenManageLecturer(false)
-            setOpenManageInstructor(false)
-            setOpenManageTopic(false)
-            setOpenManageSchedule(false)
-        }
-        if (key.key === '3') {
-            setOpenManageStudent(false)
-            setOpenAD(false)
-            setOpenManageInstructor(false)
-            setOpenManageTopic(false)
-            setOpenManageSchedule(false)
-            setOpenManageLecturer(!openManageLecturer)
-        }
-        if (key.key === '4') {
-            setOpenManageStudent(false)
-            setOpenAD(false)
-            setOpenManageLecturer(false)
-            setOpenManageTopic(false)
-            setOpenManageSchedule(false)
-            setOpenManageInstructor(!openManageInstructor)
-        }
-        if (key.key === '5') {
-            setOpenManageStudent(false)
-            setOpenAD(false)
-            setOpenManageLecturer(false)
-            setOpenManageInstructor(false)
-            setOpenManageSchedule(false)
-            setOpenManageTopic(!openManageTopic)
-        }
-        if (key.key === '8') {
-            setOpenManageStudent(false)
-            setOpenAD(false)
-            setOpenManageLecturer(false)
-            setOpenManageInstructor(false)
-            setOpenManageTopic(false)
-            setOpenManageSchedule(!openManageSchedule)
-        }
+  const openItems = (key) => {
+    if (key.key === "1") {
+      setOpenAD(!openAD);
+      setOpenManageStudent(false);
+      setOpenManageLecturer(false);
+      setOpenManageInstructor(false);
+      setOpenManageTopic(false);
     }
-    return (
-        <div>
-            <Layout >
+    if (key.key === "2") {
+      setOpenManageStudent(!openManageSetudent);
+      setOpenAD(false);
+      setOpenManageLecturer(false);
+      setOpenManageInstructor(false);
+      setOpenManageTopic(false);
+      setOpenManageSchedule(false);
+    }
+    if (key.key === "3") {
+      setOpenManageStudent(false);
+      setOpenAD(false);
+      setOpenManageInstructor(false);
+      setOpenManageTopic(false);
+      setOpenManageSchedule(false);
+      setOpenManageLecturer(!openManageLecturer);
+    }
+    if (key.key === "4") {
+      setOpenManageStudent(false);
+      setOpenAD(false);
+      setOpenManageLecturer(false);
+      setOpenManageTopic(false);
+      setOpenManageSchedule(false);
+      setOpenManageInstructor(!openManageInstructor);
+    }
+    if (key.key === "5") {
+      setOpenManageStudent(false);
+      setOpenAD(false);
+      setOpenManageLecturer(false);
+      setOpenManageInstructor(false);
+      setOpenManageSchedule(false);
+      setOpenManageTopic(!openManageTopic);
+    }
+    if (key.key === "8") {
+      setOpenManageStudent(false);
+      setOpenAD(false);
+      setOpenManageLecturer(false);
+      setOpenManageInstructor(false);
+      setOpenManageTopic(false);
+      setOpenManageSchedule(!openManageSchedule);
+    }
+  };
+  return (
+    <div>
+      <Layout>
+        <AdminHeader collapsed={collapsed} setCollapsed={setCollapsed} />
 
-                <AdminHeader
-                    collapsed={collapsed}
-                    setCollapsed={setCollapsed}
-                />
+        <Layout>
+          <Sider
+            onCollapse={(value) => setCollapsed(value)}
+            collapsible
+            collapsed={collapsed}
+            width={300}
+            style={{
+              backgroundColor: "#efefef",
+              marginLeft: -8,
+            }}
+          >
+            <Menu
+              onClick={(item, key, keyPath, domEvent) =>
+                openItems(item, key, keyPath, domEvent)
+              }
+              style={{ height: 898 }}
+              defaultSelectedKeys={["1"]}
+              mode="inline"
+              inlineCollapsed={collapsed}
+              items={items}
+            />
+          </Sider>
+          <Layout
+            style={{
+              paddingLeft: 10,
+              paddingRight: 10,
+              marginRight: -8,
+            }}
+          >
+            <Content>
+              <h1>
+                {userData.firstName} {userData.lastName} dang dang nhap
+              </h1>
+              <Button onClick={() => onSignOut()}>Đăng xuất</Button>
+              <div
+                style={{
+                  padding: 20,
+                  minHeight: 150,
+                  backgroundColor: "white",
+                }}
+              >
+                {openAD === true ? <AdminDashboard /> : ""}
+                {openManageSetudent === true ? <ManageStudent /> : ""}
+                {openManageLecturer === true ? <ManageLecturer /> : ""}
+                {openManageInstructor === true ? <ManageInstructor /> : ""}
+                {openManageTopic === true ? <ManageTopic /> : ""}
+                {openManageSchedule === true ? <ManageSchedule /> : ""}
+              </div>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    </div>
+  );
+};
 
-                <Layout>
-                    <Sider
-                        onCollapse={(value) => setCollapsed(value)}
-                        collapsible
-                        collapsed={collapsed}
-                        width={300}
-                        style={{
-                            backgroundColor: '#efefef',
-                            marginLeft: -8,
-                        }}
-                    >
-                        <Menu
-                            onClick={(item, key, keyPath, domEvent) => openItems(item, key, keyPath, domEvent)}
-                            style={{ height: 898 }}
-                            defaultSelectedKeys={['1']}
-                            mode="inline"
-                            inlineCollapsed={collapsed}
-                            items={items}
-                        />
-                    </Sider>
-                    <Layout
-                        style={{
-                            paddingLeft: 10,
-                            paddingRight: 10,
-                            marginRight: -8
-                        }}
-                    >
-
-
-                        <Content>
-                            <div
-                                style={{
-                                    padding: 20,
-                                    minHeight: 150,
-                                    backgroundColor: "white",
-                                }}
-                            >
-                                {openAD === true ? <AdminDashboard /> : ''}
-                                {openManageSetudent === true ? <ManageStudent /> : ''}
-                                {openManageLecturer === true ? <ManageLecturer /> : ''}
-                                {openManageInstructor === true ? <ManageInstructor /> : ''}
-                                {openManageTopic === true ? <ManageTopic /> : ''}
-                                {openManageSchedule === true ? <ManageSchedule /> : ''}
-                            </div>
-                        </Content>
-                    </Layout>
-                </Layout>
-            </Layout>
-
-        </div >
-    )
-}
-
-export default AdminPage
+export default AdminPage;
